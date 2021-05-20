d73d312680a774f51025a69ab30c4566d50cf226
jessalunaa <83335306+jessalunaa@users.noreply.github.com>
2021-05-20 15:28:39 +0700

    update pojects

--- function.js
+++ function.js
@@ -1,48 +1,51 @@
+function write(obj, sentence, i, cb) {
+  if (i != sentence.length) {
+    setTimeout(function () {
+      i++
+      obj.innerHTML = sentence.substr(0, i + 1) + ' <em aria-hidden="true"></em>';
+      write(obj, sentence, i, cb)
+    }, 50)
+  } else {
+    cb()
+  }
+}
 
-                  function write (obj, sentence, i, cb) {
-                    if (i != sentence.length) {
-                      setTimeout(function () {
-                        i++
-                        obj.innerHTML = sentence.substr(0, i+1) +' <em aria-hidden="true"></em>';
-                        write(obj, sentence, i, cb)
-                      }, 50)
-                    } else {
-                      cb()
-                    }
-                  }
-                   function erase (obj, cb,i) {
-                   var sentence = obj.innerText
-                      if (sentence.length != 0) {
-                       setTimeout(function () {
-                       sentence = sentence.substr(0,sentence.length-1)
-                       obj.innerText = sentence
-                       erase(obj, cb)
-                        }, 18/(i*(i/10000000)))
-                        } else {
-                        obj.innerText = " "
-                        cb()
-                     }
-                    }
-                    var typeline = document.querySelector("#typeline")
+function erase(obj, cb, i) {
+  var sentence = obj.innerText
+  if (sentence.length != 0) {
+    setTimeout(function () {
+      sentence = sentence.substr(0, sentence.length - 1)
+      obj.innerText = sentence
+      erase(obj, cb)
+    }, 18 / (i * (i / 10000000)))
+  } else {
+    obj.innerText = " "
+    cb()
+  }
+}
+var typeline = document.querySelector("#typeline")
 
-                     function writeerase(obj, sentence, time, cb) {
-                      write(obj, sentence, 0, function () {
-                       setTimeout(function () {
-                       erase(obj, cb) }, time) })
-                       }
+function writeerase(obj, sentence, time, cb) {
+  write(obj, sentence, 0, function () {
+    setTimeout(function () {
+      erase(obj, cb)
+    }, time)
+  })
+}
 
-                  var sentences = [
-                    " Luna.",
-                    "a Software Engineer.",
-                    "a Web Developer.",
-                    "a Web Designer. "
-                  ]
-                    
-                  var counter = 0
-                  function loop () {
-                    var sentence = sentences[counter % sentences.length]
-                    writeerase(typeline, sentence, 1500, loop)
-                    counter++
-                  }
+var sentences = [
+  " Luna.",
+  "a Software Engineer.",
+  "a Web Developer.",
+  "a Web Designer. "
+]
 
-                  loop()
+var counter = 0
+
+function loop() {
+  var sentence = sentences[counter % sentences.length]
+  writeerase(typeline, sentence, 1500, loop)
+  counter++
+}
+
+loop()
