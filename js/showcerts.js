0ff83fdb9657e18cfe6f3ed7089c3352fe004c1d
jessalunaa <83335306+jessalunaa@users.noreply.github.com>
2021-05-21 12:26:34 +0700

    update cert slideshow

--- js/showcerts.js
+++ js/showcerts.js
@@ -8,7 +8,9 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000);
 }
